11a7f29458e615f697cc0262d8dd607637de245f
SnowCait <SnowCait@users.noreply.github.com>
2023-04-10 23:22:38 +0900

    First follow

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import 'websocket-polyfill';
-import { SimplePool, Kind, getPublicKey, nip19 } from 'nostr-tools';
+import { SimplePool, Kind, getPublicKey, nip19, getEventHash, signEvent } from 'nostr-tools';
 
 /** @type {string} */
 const privateKey = process.env.NOSTR_PRIVATE_KEY;
@@ -23,9 +23,14 @@ const contactsEvents = await contactsPool.list(relays, [
     authors: [pubkey]
   }
 ]);
-contactsPool.close(relays);
 contactsEvents.sort((x, y) => x.created_at - y.created_at);
-console.log('[contacts]', JSON.stringify(contactsEvents));
+console.log('[contacts]', JSON.stringify(contactsEvents, null, 2));
+
+if (contactsEvents.length === 0) {
+  throw new Error('Contacts not found');
+}
+
+const contactsEvent = contactsEvents[0];
 
 const metadataPool = new SimplePool({ eoseSubTimeout: 60000 });
 const metadataEvents = await metadataPool.list(relays, [
@@ -33,6 +38,7 @@ const metadataEvents = await metadataPool.list(relays, [
     kinds: [Kind.Metadata]
   }
 ]);
+metadataPool.close(relays);
 console.log('[metadata]', metadataEvents.length);
 
 const japaneseMetadataEvents = metadataEvents
@@ -45,4 +51,26 @@ console.log('[japanese]', japaneseMetadataEvents.length, japaneseMetadataEvents.
   return `${display_name} (@${name}): ${about}`;
 }));
 
-metadataPool.close(relays);
+const pubkeys = new Set([
+  ...contactsEvent.tags.map(([,pubkey]) => pubkey),
+  ...japaneseMetadataEvents.map(x => x.pubkey)
+]);
+const event = {
+  kind: Kind.Contacts,
+  pubkey,
+  created_at: Math.floor(Date.now() / 1000),
+  tags: Array.from(pubkeys).map(pubkey => ['p', pubkey]),
+  content: contactsEvent.content
+};
+event.id = getEventHash(event);
+event.sig = signEvent(event, seckey);
+console.log('[publish]', event);
+
+const pub = contactsPool.publish(relays, event);
+pub.on('ok', relay => {
+  console.log('[ok]', relay);
+});
+pub.on('failed', relay => {
+  console.log('[failed]', relay);
+});
+setTimeout(() => contactsPool.close(relays), 3000);
