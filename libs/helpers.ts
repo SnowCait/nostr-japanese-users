c16569ab7331328adbe41452ee5dbfd16894be3b
SnowCait <SnowCait@users.noreply.github.com>
2024-03-14 18:52:11 +0900

    Replace to Deno

---
+++ libs/helpers.ts
@@ -0,0 +1,52 @@
+import dotenv from "dotenv";
+import { NostrEvent } from "npm:nostr-fetch@0.15.0";
+import { finalizeEvent, nip19 } from "npm:nostr-tools@2.3.1";
+import { hexToBytes } from "npm:@noble/hashes@1.3.2/utils";
+import relays from "../relays.json" with { type: "json" };
+import readonlyRelays from "../relays.readonly.json" with { type: "json" };
+
+dotenv.config();
+
+export const contactsJsonPath = "docs/contacts.json";
+export const sleepJsonPath = "docs/sleep.json";
+
+export const activeDays = 14;
+
+export const relayUrls = [...relays, ...readonlyRelays];
+
+export async function readEventJson(path: string): Promise<NostrEvent> {
+  const json = await Deno.readTextFile(path);
+  return JSON.parse(json);
+}
+
+export async function writeEventJson(
+  path: string,
+  kind: number,
+  tags: string[][],
+): Promise<void> {
+  const privateKey = Deno.env.get("NOSTR_PRIVATE_KEY");
+  if (privateKey === undefined) {
+    console.error("NOSTR_PRIVATE_KEY is not defined.");
+    Deno.exit(1);
+  }
+
+  const seckey = privateKey.startsWith("nsec")
+    ? nip19.decode(privateKey).data as Uint8Array
+    : hexToBytes(privateKey);
+
+  const event = await finalizeEvent({
+    kind,
+    content: "",
+    tags,
+    created_at: Math.floor(Date.now() / 1000),
+  }, seckey);
+  await Deno.writeTextFile(path, JSON.stringify(event, null, 2));
+}
+
+export function isProxy(tags: string[][]): boolean {
+  return tags.some(([tagName]) => tagName === "proxy" || tagName === "mostr");
+}
+
+export function includesJapanese(content: string): boolean {
+  return /[ぁ-ゔァ-ヺ]/u.test(content);
+}
---
+++ tasks/follow.ts
@@ -0,0 +1,110 @@
+import { parseArgs } from "https://deno.land/std@0.219.0/cli/parse_args.ts";
+import { eventKind, NostrFetcher } from "npm:nostr-fetch@0.15.0";
+import {
+  contactsJsonPath,
+  readEventJson,
+  relayUrls,
+  sleepJsonPath,
+  writeEventJson,
+} from "../libs/helpers.ts";
+import {
+  checkActive,
+  fetchJapaneseMetadataEvents,
+  manyReportedPubkeys,
+} from "../libs/nostr.ts";
+
+const options: { since: string | number | undefined } = parseArgs(Deno.args);
+
+const contacts = await readEventJson(contactsJsonPath);
+const since = options.since === undefined
+  ? contacts.created_at
+  : Math.floor(new Date(options.since).getTime() / 1000);
+console.log(
+  "[last followees]",
+  contacts.tags.length,
+  new Date(since * 1000).toString(),
+);
+
+const fetcher = NostrFetcher.init();
+
+const japanesMetadataEvents = await fetchJapaneseMetadataEvents(
+  fetcher,
+  relayUrls,
+  since,
+);
+const newPubkeys = [
+  ...new Set(
+    japanesMetadataEvents.filter((event) =>
+      !contacts.tags.some(([tagName, pubkey]) =>
+        tagName === "p" && pubkey === event.pubkey
+      )
+    ).map(
+      (event) => event.pubkey,
+    ),
+  ),
+];
+console.log(
+  "[new japanese]",
+  newPubkeys.length,
+  newPubkeys,
+  japanesMetadataEvents.map((event) => event.content),
+);
+
+const reportedPubkeys = await manyReportedPubkeys(
+  fetcher,
+  relayUrls,
+  newPubkeys,
+);
+const pubkeys = newPubkeys.filter((pubkey) =>
+  !reportedPubkeys.includes(pubkey)
+);
+console.log("[japanese]", pubkeys.length, pubkeys);
+
+const pubkeysMap = await checkActive(fetcher, relayUrls, pubkeys);
+const addingPubkeys = [...pubkeysMap].filter(([, active]) => active).map((
+  [pubkey],
+) => pubkey);
+console.log("[active japanese]", addingPubkeys.length, pubkeysMap);
+
+const inactiveOrProxyPubkeys = [...pubkeysMap].filter(([, active]) => !active)
+  .map(([pubkey]) => pubkey);
+console.log("[inactive/proxy japanese]", inactiveOrProxyPubkeys.length);
+
+fetcher.shutdown();
+
+if (addingPubkeys.length > 0) {
+  const tags = [
+    ...contacts.tags,
+    ...addingPubkeys.map((pubkey) => ["p", pubkey]),
+  ];
+  console.log("[followees]", tags.length);
+  await writeEventJson(contactsJsonPath, eventKind.contacts, tags);
+}
+
+if (inactiveOrProxyPubkeys.length > 0) {
+  const sleep = await readEventJson(sleepJsonPath);
+  const addingSleeper = inactiveOrProxyPubkeys.filter((pubkey) =>
+    !sleep.tags.some(([tagName, p]) => tagName === "p" && pubkey === p)
+  );
+  console.log(
+    "[sleep]",
+    addingSleeper.length,
+    inactiveOrProxyPubkeys.length,
+    addingSleeper,
+  );
+  if (addingSleeper.length > 0) {
+    const tags = [
+      ...sleep.tags,
+      ...addingSleeper.map((pubkey) => ["p", pubkey]),
+    ];
+    console.log(
+      "[sleepers]",
+      tags.filter(([tagName]) => tagName === "p").length,
+    );
+    await writeEventJson(
+      sleepJsonPath,
+      eventKind.categorizedPeopleList,
+      tags,
+    );
+  }
+}
